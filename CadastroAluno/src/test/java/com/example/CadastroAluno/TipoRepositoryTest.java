c464f37da444dea735686e5cfd5d770e073b21bd
José Luiz <jluizbc94@gmail.com>
2023-12-22 18:10:34 -0300

    projeto finalizado

---
+++ CadastroAluno/src/test/java/com/example/CadastroAluno/TipoRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.CadastroAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.example.CadastroAluno.Model.Tipo;
+import com.example.CadastroAluno.Repository.TipoRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class TipoRepositoryTest {
+
+    @Autowired
+    private TipoRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void TestCreatedTipo(){
+        Tipo tipo = new Tipo();
+
+        tipo.setTipo("Leve");
+        tipo.setTipo("Medio");
+        tipo.setTipo("Pesado");
+
+        Tipo savedTipo = repo.save(tipo);
+
+        Tipo existTipo = entityManager.find(Tipo.class, savedTipo.getId());
+
+        assert(existTipo.getTipo().equals(tipo.getTipo()));
+    }
+
+}
